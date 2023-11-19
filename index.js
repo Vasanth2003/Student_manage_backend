102e65a47cb9393b07a5beb330233866ad71f419
Vasanth <87744906+Vasanth2003@users.noreply.github.com>
2023-11-20 01:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import studentRouter from './routes/student_routes.js';
 import dotenv from 'dotenv';
-import { addStudent, deleteById,searchById, deleteAll,getAllStudents, updateStudentById } from "./controllers/studentController.js";
-
-
 dotenv.config();
 
 const PORT = process.env.PORT || 5000; // Use the provided port or default to 5000
@@ -14,12 +12,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.post("/add ",addStudent);
-app.get("/students",getAllStudents);
-app.put("/updatedetails/:id",updateStudentById);
-app.delete("/deletestudent/:id",deleteById);
-app.delete("/deleteall",deleteAll);
-app.get("/search/:id",searchById);
+app.use('/api/student', studentRouter);
+
+
 // MongoDB URI from .env file
 const mongodbURI = process.env.MONGODB_URI;
 
