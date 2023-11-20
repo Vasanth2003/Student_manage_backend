5a1e76d7c3a22550fe334ee5754e26154e597226
Vasanth <87744906+Vasanth2003@users.noreply.github.com>
2023-11-20 10:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000; 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: 'https://student-management-mauve.vercel.app/',
+  })
+);
 
 app.use(express.json());
 app.use('/api/student', studentRouter);
